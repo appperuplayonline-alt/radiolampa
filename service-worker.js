d6448d9f55f849a5cc104f95e6698fb8f795d3a8
appperuplayonline-alt <appperuplayonline@gmail.com>
2025-10-27 13:46:04 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'radio-lampa-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  'https://i.postimg.cc/VNbNzVqB/IMG-20251027-WA0016.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request).then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+    ).catch(() => caches.match('./index.html'))
+  );
+});
